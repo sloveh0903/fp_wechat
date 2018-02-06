54a661463019bb1d9fecfeafeea74fdc659a8f79
sloveh0903 <sloveh0903@sina.cn>
2018-02-06 17:58:49 +0800

    footprint wechat api test

---
+++ src/main/java/cn/sun/demo/FpWechatApplication.java
@@ -0,0 +1,12 @@
+package cn.sun.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FpWechatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FpWechatApplication.class, args);
+	}
+}
